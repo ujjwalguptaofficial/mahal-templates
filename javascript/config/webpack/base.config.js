549ab0c26b0f52cc5e6c360345ac3db95a97362b
Ujjwal Gupta <ujjwalkumargupta4u@gmail.com>
2022-03-16 11:12:32 +0530

    change mahal logo and move webpack config to config folder

--- javascript/config/webpack/base.config.js
+++ javascript/config/webpack/base.config.js
@@ -4,7 +4,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
-const rootFolder = path.join(__dirname, '../');
+const rootFolder = path.join(__dirname, '../../');
 
 module.exports = {
     entry: './src/index.js',
