8bd983f11fc8a04bde75aad61bedc1c9e78422b9
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2022-08-24 21:47:43 +0530

    setup tests in javascript

---
+++ javascript/tests/jsdom.setup.js
@@ -0,0 +1,21 @@
+const jsdom = require('jsdom');
+
+// console.log(process.env.NODE_ENV)
+
+const jsdomInstance = new jsdom.JSDOM(`<!DOCTYPE html>
+<html lang="en"><body> <div id="app"></div></body></html>`, {
+    url: "https://example.org/"
+});
+global.window = jsdomInstance.window;
+global.document = window.document;
+window.console = global.console;
+
+Object.keys(document.defaultView).forEach((property) => {
+    if (typeof global[property] === 'undefined') {
+        global[property] = document.defaultView[property];
+    }
+});
+
+global.navigator = {
+    userAgent: 'node.js'
+};
