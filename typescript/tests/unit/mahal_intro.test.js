e398173563ba03dc3b5200cff0b6fa19748d4b81
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2022-08-24 21:58:07 +0530

    setup tests for typescript

---
+++ typescript/tests/unit/mahal_intro.test.js
@@ -0,0 +1,16 @@
+import { initiate, mount, getMount } from "@mahaljs/test-utils";
+import MahalIntro from "@/components/mahal_intro.mahal";
+import { expect } from "chai";
+
+
+describe("Mahal Intro", () => {
+    let component;
+    before(async () => {
+        component = await mount(MahalIntro);
+    });
+
+    it("check h1", async () => {
+        const h1 = component.find('h1');
+        expect(h1.innerHTML).equal(`Welcome to the Mahal App`);
+    });
+});
